3678e38ddaca227f6d8c6312c8a3876802f3799d
Kaja Solberg <kajasolberg@Kajas-MacBook-Pro.local>
2020-07-23 11:23:12 +0200

    updated Toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,18 +1,15 @@
 import React from "react";
-import {container, Container} from 'reactstrap';
 
 import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
 import "./Toolbar.css";
 
 const toolbar = (props) => (
-  <Container>
   <header className="toolbar">
     <nav className="toolbar__navigation">
       <div className="toolbar__toggle-button">
         <DrawerToggleButton click={props.drawerClickHandler} />
       </div>
       <div className="toolbar__logo">
-      {/* <img src={Windmill} alt="Windmill" />; */}
         <a href="/">Green Forecast</a>
       </div>
       <div className="spacer" />
@@ -28,7 +25,6 @@ const toolbar = (props) => (
       </div>
     </nav>
   </header>
-  </Container>
 );
 
 export default toolbar;
